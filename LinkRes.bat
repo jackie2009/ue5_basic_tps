a9da87bda52ca81e78e0184265ae32bbec726228
jackie2009 <jackie_2009@163.com>
2026-06-01 14:51:37 +0800

    dll改为拷贝模式

--- LinkRes.bat
+++ LinkRes.bat
@@ -1,2 +1,2 @@
 net use \\192.168.0.239\InfiniteRes /user:InfiniteRes InfiniteRes
-mklink /D Binaries \\192.168.0.239\InfiniteRes  
+robocopy "\\192.168.0.239\InfiniteRes" "Binaries" /IS /E /PURGE /R:1 /W:1 /MT:16 /NDL /NFL
